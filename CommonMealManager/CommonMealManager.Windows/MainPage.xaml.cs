0eee7421da175abd8f04a0f09a6342cd1324ca32
David <dlombet@hotmail.com>
2014-11-10 16:30:14 +0100

    test de mon coté
    réussi?

--- CommonMealManager/CommonMealManager.Windows/MainPage.xaml.cs
+++ CommonMealManager/CommonMealManager.Windows/MainPage.xaml.cs
@@ -20,6 +20,7 @@ namespace CommonMealManager
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
+	//test
     public sealed partial class MainPage : Page
     {
         public MainPage()
